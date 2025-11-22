1256c72713c27804f68da89db9c4496922eb43dc
Talha9685 <alqama786khan@gmail.com>
2025-11-22 05:40:26 +0530

    Add files via upload

---
+++ github.py
@@ -0,0 +1,15 @@
+#1) Accept numbers from a user
+a = int(input("Enter your number:"))
+print(f"Your number is {a}")
+
+#------------------------------------------
+# 2) Display three string “Name”, “Is”, “James” as “Name**Is**James”
+a = "Name"
+b = "Is"
+c = "James"
+print(a + "**" + b + "**" + c)
+
+#------------------------------------------
+# 3) Display float number with 2 decimal places using print()
+x = float(input("Enter your number:"))
+print(f"your number is {round(x,2)}")
