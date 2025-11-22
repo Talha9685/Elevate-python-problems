d8ba256f975f4f54a9b4a58bb0499092daca88c1
Talha9685 <alqama786khan@gmail.com>
2025-11-22 05:46:36 +0530

    Update github.py

--- github.py
+++ github.py
@@ -13,3 +13,10 @@ print(a + "**" + b + "**" + c)
 # 3) Display float number with 2 decimal places using print()
 x = float(input("Enter your number:"))
 print(f"your number is {round(x,2)}")
+
+#-----------------------------------------
+#4) Sum of two integers
+x = int(input("Enter your first  number:"))
+y = int(input("Enter your second number:"))
+z= x+y
+print(f'The sum of your given number is {x+y}') or print(f"The sum of your given number is {z}")
